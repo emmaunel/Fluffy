3112a7b79a27beb7a513dc4695a0910e6c7582a5
Emmanuel <aadewale123@gmail.com>
2018-12-20 15:06:15 -0500

    Working on the Lexer

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
 # Fluffy_Language
-This is my own interpreter programming called fluffy
+This is my own interpreter programming called Fluffy.
+
+#Background
+Fluffy was inspired by the language 'Chicken' which is an esoteric programming language.
+I wanted Fluffy to be just like chicken except its tokens are different varient of 
+the word Fluffy(e.g Fluffy, fluffY, FLuffy). But at the moment, it would be like
+a regular programming language with variable names, functions, and so on.
 
 # TODO
 
 * Create a Lexer or use an open source Tool
     Status
 	* Research(possibly gonna write in python)
+	* Interesting thing, the tokens are put into a tree(glad I paid attention in CS141)
 * Create a Parser
 * That's it for now
--- lexer.py
+++ lexer.py
@@ -1,1 +1,42 @@
-print("Hello World!")
+from sly import Lexer
+
+class BasicLexer(Lexer):
+    tokens = {NAME, NUMBER, STRING, IF, ELSE, FOR, FUNC, EQEQ}
+    ignore = '\t'
+
+    #Define Tokens
+    NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
+    IF = r'if'
+    ELSE = r'else'
+    FOR = r'for'
+    FUNC = r'func'
+    STRING = r'\".*?\"'
+
+    EQEQ = r'=='
+
+    @_(r'\d+')
+    def NUMBER(self, t):
+        t.value = int(t.value)
+        return t
+
+    @_(r'//.*')
+    def COMMENT(self, t):
+        pass
+
+    @_(r'\n+')
+    def newline(self, t):
+        self.lineno = t.value.count('\n')
+
+
+if __name__ == '__main__':
+    lexer = BasicLexer()
+    env = {}
+    while True:
+        try:
+            text = input('fluffy > ')
+        except EOFError:
+            break
+        if text:
+            lex = lexer.tokenize(text)
+            # for token in lex:
+            #     print(token)
