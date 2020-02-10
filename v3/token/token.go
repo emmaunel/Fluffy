d2776230ae1b45b03801cda5cb2647192d1e0038
Emmanuel <aadewale123@gmail.com>
2020-02-10 09:14:03 -0500

    switching to go

---
+++ v3/token/token.go
@@ -0,0 +1,39 @@
+package token
+
+const (
+
+	// Data types
+	int
+	str
+	bool
+	void
+	char
+
+	// statement endings
+	Newline
+	Semicolon
+
+	// Operators
+	OpenCurly
+	CloseCurly
+	Arrow
+
+	// Mahtematical operators
+	Plus
+	Minus
+	Divide
+	Times
+
+	Increment
+	Decrement
+
+	// Keyword
+	If
+	Elif
+	Else
+	For
+	Break
+	Continue
+
+
+)
